25758a1e582f6a562a1a90b1da9d5f86dcd93116
David Junior <102331951+Zaarth@users.noreply.github.com>
2024-08-20 23:32:11 +0000

    Códigos aula 04

---
+++ aula04/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+
+    public static void Main(string [] args){
+        Pessoa objPessoa = new Pessoa("David");
+        Console.WriteLine("Antes do método (Classe): " + objPessoa.ToString());
+        AlteraNomeClasse(objPessoa);
+        Console.WriteLine("Depois do método (Classe): " + objPessoa.ToString());
+
+        PessoaStruct pessoaStruct = new PessoaStruct("Arthur");
+        Console.WriteLine("Antes do método (struct): " + pessoaStruct.ToString());
+        AlteraNomeStruct(pessoaStruct);
+        Console.WriteLine("Depois do método (struct): " + pessoaStruct.ToString());
+    }
+
+    public static void AlteraNomeClasse(Pessoa objAux){
+        objAux.Nome = "Felipe";
+    }
+
+    public static void AlteraNomeStruct(PessoaStruct objAux){
+        objAux.NomeStruct = "Isadora";
+    }
+}
