cc47d201f787f4c56f34259b4e79f33a6e86589d
David Junior <102331951+Zaarth@users.noreply.github.com>
2025-05-10 00:28:43 +0000

    Api

--- Mobile/aulaMongo/projetoApi/src/routes/produtoRoutes.js
+++ Mobile/aulaMongo/projetoApi/src/routes/produtoRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { produtocontroller } from '../controllers/produtocontroller';
+import { produtoController } from '../controllers/produtoController.js';
 
 const router = Router();
-router.post('/', produtocontroller.criar);
-router.get('/', produtocontroller.listar);
-router.get('/', produtocontroller.obter);
-router.put('/', produtocontroller.atualizar);
-router.delete('/', produtocontroller.deletar);
+router.post('/',   produtoController.criar);
+router.get('/',    produtoController.listar);
+router.get('/:id', produtoController.obter);
+router.put('/:id', produtoController.atualizar);
+router.delete('/:id', produtoController.deletar);
 
 export default router;
